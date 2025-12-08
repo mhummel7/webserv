72cb1616de3efcb2369d10dc61f68eb9595f9af2
¨mhummel7¨ <mhummel@student.42heilbronn.de>
2025-12-08 10:43:23 +0100

    Translated the html etc. to english

--- root/cgi-bin/hello.php
+++ root/cgi-bin/hello.php
@@ -1,5 +1,5 @@
 <?php
 echo "Content-Type: text/html\n\n";
 echo "<h1>Hello from PHP CGI!</h1>";
-echo "<p>Es funktioniert!</p>";
+echo "<p>It works!</p>";
 ?>
--- root/cgi-bin/time.py
+++ root/cgi-bin/time.py
@@ -1,6 +1,6 @@
 import datetime
 
 print("<html><body>")
-print("<h1>Aktuelle Uhrzeit</h1>")
+print("<h1>Current Time</h1>")
 print("<p>{}</p>".format(datetime.datetime.now()))
 print("</body></html>")
