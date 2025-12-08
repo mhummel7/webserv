443caaba016083b7359a42918d1cf4b17c634daa
¨mhummel7¨ <mhummel@student.42heilbronn.de>
2025-12-08 10:45:30 +0100

    No miliseconds

--- root/cgi-bin/time.py
+++ root/cgi-bin/time.py
@@ -1,6 +1,7 @@
 import datetime
 
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print("<html><body>")
 print("<h1>Current Time</h1>")
-print("<p>{}</p>".format(datetime.datetime.now()))
+print("<p>{}</p>".format(now))
 print("</body></html>")
